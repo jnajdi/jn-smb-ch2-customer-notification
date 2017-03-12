876e27283680b45f18bcc7044118b8af91e00263
Jihane Najdi <jnajdi@gmail.com>
2017-03-12 09:00:49 -0700

    Renamed Application class

--- src/test/java/jnajdi/sandbox/example/CustomerNotificationApplicationTests.java
+++ src/test/java/jnajdi/sandbox/example/CustomerNotificationApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JnSmbCh2CustomerNotificationApplicationTests {
+public class CustomerNotificationApplicationTests {
 
 	@Test
 	public void contextLoads() {
