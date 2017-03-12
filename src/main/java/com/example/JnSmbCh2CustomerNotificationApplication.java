abb0d070f84cbea89c8e2a49e386ba494204870d
Jihane Najdi <jnajdi@gmail.com>
2017-03-12 08:45:55 -0700

    Initial commit

---
+++ src/main/java/com/example/JnSmbCh2CustomerNotificationApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnSmbCh2CustomerNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JnSmbCh2CustomerNotificationApplication.class, args);
+	}
+}
